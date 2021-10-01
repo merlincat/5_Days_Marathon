d8c51809cd313cf2bd77232ce110af44bce1c4a8
Nata <merlincat.no@gmail.com>
2021-10-01 12:37:26 +0300

    swipe by controls

--- 03-slider-sources/app.js
+++ 03-slider-sources/app.js
@@ -15,6 +15,13 @@ upBtn.addEventListener('click', () => {
 downBtn.addEventListener('click', () => {
     changeSlide('down')
 })
+document/addEventListener('keydown', event => {
+    if (event.key ==='ArrowUp'){
+        changeSlide('up')
+    }else if (event.key === 'ArrowDown'){
+        changeSlide('down')
+    }
+})
 
 function changeSlide(direction){
     if(direction === 'up'){
